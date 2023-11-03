c30d403c983184bfa1b05d8c3ea5d269f363f686
Vaishnavi Sen <vaishnavisen@Vaishnavis-MBP.lan>
2023-11-02 21:45:31 -0700

    generate_puzzle done

--- solution.java
+++ solution.java
@@ -10,7 +10,15 @@ public class solution {
     public static int threadCCount[] = new int[66];
 
     public static void generatePuzzle() {
-
+        while (i <= 65*3) {
+            output = 1 + (int)(Math.floor(17 * Math.E * k) % 65);
+            if (count[output] < 3) {
+              puzzle[i-1] = output; 
+                i++;
+                count[output]++;
+            }
+            k++; 
+        }
     }
 
     public static void printPuzzle() {
